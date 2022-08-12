deec584f1e36be3f46ba42c6ce67cdc6a3337cdc
Camilo Arriagada Vallejos <95378920+c4miloarriagada@users.noreply.github.com>
2022-08-12 18:14:29 -0400

    animations

--- 08-journal-app/src/auth/pages/RegisterPage.jsx
+++ 08-journal-app/src/auth/pages/RegisterPage.jsx
@@ -53,7 +53,9 @@ export const RegisterPage = () => {
 
   return (
     <AuthLayout title="Crear cuenta">
-      <form onSubmit={onSubmit}>
+      <form 
+      className="animate__animated animate__fadeIn animate__faster"
+      onSubmit={onSubmit}>
         <Grid container>
           <Grid item xs={12} sx={{ mt: 2 }}>
             <TextField
