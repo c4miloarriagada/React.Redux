0097ab1fffcee9de9ae5621bef9aad6af01a8316
Camilo Arriagada Vallejos <95378920+c4miloarriagada@users.noreply.github.com>
2022-08-16 20:12:10 -0400

    calendar-app

---
+++ 10-Calendar-App/src/calendar/pages/CalendarPage.jsx
@@ -0,0 +1,75 @@
+import { Calendar } from 'react-big-calendar'
+import { localizer, getMessagesES } from '../helpers'
+import { addHours } from 'date-fns'
+import 'react-big-calendar/lib/css/react-big-calendar.css'
+import { Navbar, CalendarEvent } from "../"
+import { useState } from 'react'
+
+
+const events = [{
+  title: 'cumpleaños',
+  notes: 'hay que comprar pastel',
+  start: new Date(),
+  end: addHours( new Date(), 2 ),
+  bgColor: '#fafafa',
+  user: {
+    _id: '123',
+    name: 'Camilo'
+  }
+}]
+
+export const CalendarPage = () => {
+
+  const [lastView, setlastView] = useState(localStorage.getItem('lastView') || 'week');
+
+  const eventStyleGetter = (event, start, end, isSelected) => {
+    
+    const style = {
+      backgroundColor: '#347CF7',
+      borderRadius: '0px',
+      opacity: 0.8,
+      color: 'white'
+    }
+    return {
+      style
+    }
+  }
+
+  const onDoubleClick = ( event ) => {
+    console.log({doubleClick: event})
+  }
+  const onSelect = ( event ) => {
+    console.log({click: event})
+  }
+  const onViewChanged = ( event ) => {
+    localStorage.setItem('lastView', event);
+    setlastView( event )
+  }
+  
+
+
+  return (
+    <>
+    <Navbar/>
+    <Calendar
+    culture='es'
+      localizer={localizer}
+      events={events}
+      defaultView={ lastView }
+      startAccessor="start"
+      endAccessor="end"
+      style={{ height: 'calc(100vh - 80px)'}}
+      messages={getMessagesES()}
+      eventPropGetter={ eventStyleGetter }
+      components={{
+        event: CalendarEvent 
+      }}
+      onDoubleClickEvent={ onDoubleClick }
+      onSelectEvent={ onSelect }
+      onView={ onViewChanged }
+
+    />
+
+    </>
+  )
+}
