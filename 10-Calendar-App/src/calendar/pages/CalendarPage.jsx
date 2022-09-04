1d2f96c09d98e02baf30af696e8badaea345bc49
Camilo Arriagada Vallejos <95378920+c4miloarriagada@users.noreply.github.com>
2022-09-04 12:19:23 -0300

    modal

--- 10-Calendar-App/src/calendar/pages/CalendarPage.jsx
+++ 10-Calendar-App/src/calendar/pages/CalendarPage.jsx
@@ -2,7 +2,7 @@ import { Calendar } from 'react-big-calendar'
 import { localizer, getMessagesES } from '../helpers'
 import { addHours } from 'date-fns'
 import 'react-big-calendar/lib/css/react-big-calendar.css'
-import { Navbar, CalendarEvent } from "../"
+import { Navbar, CalendarEvent, CalendarModal } from "../"
 import { useState } from 'react'
 
 
@@ -69,7 +69,7 @@ export const CalendarPage = () => {
       onView={ onViewChanged }
 
     />
-
+    <CalendarModal/>
     </>
   )
 }
